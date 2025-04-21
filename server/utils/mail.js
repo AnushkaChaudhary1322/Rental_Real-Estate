61e9defb4b7df15654a48ad850f839f4407099dd
AnushkaChaudhary1322 <anushka1322.be22@chitkara.edu.in>
2025-04-21 23:13:37 +0530

    mail backend

---
+++ server/utils/mail.js
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+
+
+export const sendMail = async (to, subject, text) => {
+    const transporter = nodemailer.createTransport({
+        // service: "gmail",
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.NODEMAILER_USER,
+            pass: process.env.NODEMAILER_PASS,
+        },
+        });
+    await transporter.sendMail({
+        from: process.env.NODEMAILER_USER,
+        to,
+        subject,
+        text,
+    });
+};
